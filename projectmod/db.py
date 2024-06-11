525e91558e235c015447b6b26d7d43980d46d9d5
sebastian2000pk <juochoas@unal.edu.co>
2024-06-11 00:03:09 -0500

    feat: database connection

---
+++ projectmod/db.py
@@ -0,0 +1,18 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+DATABASE_CONNECTION = "sqlite:///inventory.db"
+
+engine = create_engine(DATABASE_CONNECTION)
+
+Session = sessionmaker(bind=engine)
+
+Base = declarative_base()
+
+def get_database_session():
+    db = Session()
+    try:
+      yield db
+    finally:
+      db.close()
