cfc2cdc75252efa21db7398f1a111a4369113436
sebastian2000pk <juochoas@unal.edu.co>
2024-06-11 02:49:03 -0500

    feat(tables): create and get

--- projectmod/api/endpoints/tables.py
+++ projectmod/api/endpoints/tables.py
@@ -1,13 +1,20 @@
 from fastapi import APIRouter
+from projectmod.models.table import TableCreate
 from projectmod.services.tables import TableService
 from projectmod.database.db import get_database_session
 
 router = APIRouter()
 
+db = get_database_session()
 
 @router.get("/")
 async def get_tables():
-    db = get_database_session()
     table_service = TableService(db)
     result = table_service.get_all_tables()
+    return result
+
+@router.post("/")
+async def create_table(table: TableCreate):
+    table_service = TableService(db)
+    result = table_service.create_table(table)
     return result
