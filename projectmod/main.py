51e4a6dddd6f3c977ebc5b0248d07ff04c59c316
sebastian2000pk <juochoas@unal.edu.co>
2024-06-10 23:13:39 -0500

    api router

--- projectmod/main.py
+++ projectmod/main.py
@@ -8,12 +8,15 @@ from .sale_subsystem import Product
 # from pydantic import BaseModel, SecretStr
 # from dotenv import load_dotenv
 # from sqlalchemy import create_engine
+from .api import router as api_router
 
 app = FastAPI(
     title="Sales Managment Project",
     version="0.1",
     description="This is an api web to use services for sales managment.",
 )
+
+app.include_router(api_router, prefix="/api")
 
 class LoginInfo(BaseModel):
     name: str
