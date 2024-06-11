d30b88fee37a470228ef4e4768053260becbb171
sebastian2000pk <juochoas@unal.edu.co>
2024-06-10 23:27:00 -0500

    add: sales endpoint

--- projectmod/api/router.py
+++ projectmod/api/router.py
@@ -1,10 +1,12 @@
 from fastapi import APIRouter
 #routes
 from .endpoints.products import router as products_router
+from .endpoints.sales import router as sales_router
 
 api_router = APIRouter()
 
 api_router.include_router(products_router, prefix="/products", tags=["products"])
+api_router.include_router(sales_router, prefix="/sales", tags=["sales"])
 
 @api_router.get("/hello-world")
 async def hello_world():
